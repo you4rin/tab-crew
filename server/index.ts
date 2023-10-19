299e5b73462b995d13a34fce8f7a6114768ff4fc
you4rin <artichoke42@naver.com>
2023-10-20 00:05:31 +0900

    add card shuffle logic

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,15 @@ import asyncHandler from 'express-async-handler'
 
 const app = express();
 const prisma = new PrismaClient();
+const cards = ["r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9",
+               "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9",
+               "y1", "y2", "y3", "y4", "y5", "y6", "y7", "y8", "y9",
+               "g1", "g2", "g3", "g4", "g5", "g6", "g7", "g8", "g9",
+               "k1", "k2", "k3", "k4"];
+               
+function shuffle_cards() {
+  cards.sort(() => Math.random() - 0.5);
+}
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err.stack)
